d400b2e75b13a821cdf501e123ce90a0e1014e50
cocorocho <cobanerkan@yahoo.com>
2023-10-05 21:36:56 -0300

    add paginator

---
+++ client/components/navigation/Paginator.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="join py-4">
+    <NuxtLink
+      v-for="page in numPages"
+      :to="{ query: { page: page }}"
+      class="join-item btn btn-sm"
+      :class="{
+        'btn-active': Number(currentPage) === page,
+      }"
+      @click="onPageChanged(page)"
+    >
+      {{ page }}
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  currentPage: string | number,
+  numPages: number,
+  url: string,
+  onPageChange?: Function
+}>();
+
+const maxShownButtons: number = 5;
+
+const onPageChanged = (page: number) => {
+  if (props.onPageChange && page !== Number(props.currentPage)) {
+    props.onPageChange();
+  }
+}
+
+// TODO pagination for lots of pages
+</script>
